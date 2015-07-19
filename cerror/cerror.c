6a3428a74b8c6d7d3aa3199cce21ea96b08d426c
Darren Smith <git@darrenjs.net>
2015-07-19 21:47:40 +0100

    set to GPLV3 licence

--- cerror/cerror.c
+++ cerror/cerror.c
@@ -1,9 +1,28 @@
+/*
+    Copyright 2015, Darren Smith
+
+    This file is part of c_dev_utils.
+
+    c_dev_utils is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    c_dev_utils is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with c_dev_utils.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define OUT( X ) \
-  printf(#X " %d \n", X )
+  printf("%s, %d, %s\n", #X, X, strerror( X ) )
 
 int main(int argc , const char** argv)
 {
